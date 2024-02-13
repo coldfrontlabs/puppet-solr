e1717332707926e51f25655377804367f1b1b8e4
Mathew Winstone <mathew.winstone@gmail.com>
2024-02-13 16:24:40 -0500

    set solr user group

--- manifests/install.pp
+++ manifests/install.pp
@@ -20,6 +20,7 @@ class solr::install {
     home       => $solr::var_dir,
     system     => true,
     managehome => true,
+    groups     => $solr::solr_user,
     shell      => '/bin/bash',
     require    => Package[$solr::required_packages],
   }
